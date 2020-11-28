450c549a007d6483683693aa804cc1e218bce2de
Diana Utan <didiutzi@gmail.com>
2020-11-28 17:30:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,33 +4,30 @@ def badge_maker(name)
 end
   
 def batch_badge_creator(names)
-           # create empty array to push new messages
+  # create empty array to push new messages
   badge_messages = []
-            # we iterate our names array 
+  # we iterate our names array 
   names.each do |name| 
     message = badge_maker(name)
     badge_messages << message
   end
-       # we return the new array
+  # we return the new array
   badge_messages
 end
 
 def assign_rooms(speakers)
-             # we declare room number starting at #1
+  # we declare room number starting at #1
   room_number = 1
-  
-            # we create empty array that return the message
+  # we create empty array that return the message
   room_message=[]
-  
-            # loop thru our speakers array
+  # loop thru our speakers array
   speakers.each do |name|  
-           # push the message to our empty array
-  room_message << "Hello, #{name}! You'll be assigned to room #{room_number}!"
-             # we increment room number
-  room_number +=1
-  
-end
-          # we return room_message
+    # push the message to our empty array
+    room_message << "Hello, #{name}! You'll be assigned to room #{room_number}!"
+    # we increment room number
+    room_number +=1
+  end
+  # we return room_message
   room_message
 end
 
